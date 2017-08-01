8868200b0325fcf3049e66bafa8748c37d982aca
Ryan <ryan.a.opel@gmail.com>
2017-08-01 15:21:51 -0700

    Documentation causing LazyLoad error

--- R/replace_na.R
+++ R/replace_na.R
@@ -2,13 +2,13 @@
 #'
 #' Replace all NA's with another value in a data.frame or matrix.
 #' @param dat A data.frame or matrix
-#' @param repl.val Replacement value. Defaults to 0.
-#' @usage replace_na(dat, repl.val)
+#' @param repl Replacement value. Defaults to 0.
+#' @usage replace_na(dat, repl)
 #' @export
-#' @examples replace_na(matrix(c(1,NA,3,4), ncol = 2), repl.val = 0)
+#' @examples replace_na(matrix(c(1,NA,3,4), ncol = 2), repl = 0)
 
 
-replace_na <- function(dat, repl.val = 0) {
-  dat[is.na(dat)] <- repl.val
+replace_na <- function(dat, repl = 0) {
+  dat[is.na(dat)] <- repl
   return(dat)
 }
