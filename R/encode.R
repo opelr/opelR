2b9ae675fb2f03f9cdffb6c7ca50bcc7248f9019
Ryan Opel <ryan.a.opel@gmail.com>
2018-04-29 12:03:39 -0700

    Updated fn arguments to allow return of either a vector or a split-list object.

--- R/encode.R
+++ R/encode.R
@@ -2,16 +2,21 @@
 #'
 #' When passed a vector of discontinuous numbers, this function returns a list of each continuous sequence
 #' @param x A vector or an R object, which is coercible to one by as.vector(x, "numeric")
-#' @param print logical. Should the list be printed to the console?
-#' @usage encode(x, print = FALSE)
+#' @param as.list logical. Should the vector be returned as a split list?
+#' @usage encode(x, as.list = FALSE)
 #' @export
-#' @examples encode(c(1:4, 7:10))
+#' @examples encode(c(1:4, 7:10), as.list = FALSE)
+#' @examples encode(c(1:4, 7:10), as.list = TRUE)
 
-encode<-function(x){
-  rr <- rle(x - seq_along(x))
-  rr$values <- seq_along(rr$values)
-  s <- split(x, inverse.rle(rr))
+encode <- function(x, as.list = FALSE){
+  run_lengths <- rle(x - seq_along(x))
+  run_lengths$values <- seq_along(run_lengths$values)
+  i_rle <- inverse.rle(run_lengths)
 
-  return(s)
+  if (as.list) {
+    return(split(x, i_rle))
+  } else{
+    return(i_rle)
+  }
 }
 
